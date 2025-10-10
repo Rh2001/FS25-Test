3385c0c7f57feb356a1564a385bbff23c14ed2dd
Roham H <100292931+Rh2001@users.noreply.github.com>
2025-10-11 00:12:01 +0300

    Add Initial Files

---
+++ TestApp/Program.cs
@@ -0,0 +1,41 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Adding CORS support to our backend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+    builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+/*// Add services to the container.
+builder.Services.AddControllersWithViews();*/
+
+var app = builder.Build();
+app.UseCors("AllowReactApp");
+
+app.Run();
+
+/*
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+
+app.Run();*/
